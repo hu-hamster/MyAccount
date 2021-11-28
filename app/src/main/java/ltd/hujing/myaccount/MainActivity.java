b6b5cd8735e1a32678012f254cf480a282eef1a3
hu-hamster <hj.325915>
2021-11-28 17:10:23 +0800

    修改了原有布局，添加了两个Activity，接下来工作把主Activity的布局实现

--- app/src/main/java/ltd/hujing/myaccount/MainActivity.java
+++ app/src/main/java/ltd/hujing/myaccount/MainActivity.java
@@ -1,57 +1,59 @@
 package ltd.hujing.myaccount;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.annotation.NonNull;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
-import ltd.hujing.myaccount.databinding.ActivityMainBinding;
-import ltd.hujing.myaccount.ui.home.HomeFragment;
+
+import ltd.hujing.myaccount.ui.history.HistoryActivity;
+import ltd.hujing.myaccount.ui.home.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
-    private HomeFragment homeFragment;
-
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.appBarMain.toolbar);
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_front, R.id.nav_history, R.id.nav_analyse)
-                .setOpenableLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @SuppressLint({"NonConstantResourceId", "RtlHardcoded"})
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.nav_front:
+                                intent = new Intent(MainActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                                break;
+                            case R.id.nav_history:
+                                intent = new Intent(MainActivity.this, HistoryActivity.class);
+                                startActivity(intent);
+                                break;
+                            case R.id.nav_analyse:
+                                break;
+                            case R.id.nav_person:
+                                break;
+                        }
+                        drawerLayout.closeDrawer(Gravity.LEFT);
+                        return false;
+                    }
+                });
 
     }
 
@@ -64,9 +66,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.menu_to_top)
+            Toast.makeText(this, "test",Toast.LENGTH_SHORT).show();
+        return true;
     }
+
 }
